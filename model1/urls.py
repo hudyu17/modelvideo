02a2c440e4de7d4eee5fc6dc151a89326f7b591b
hudyu17 <hengjeung.yuen@gmail.com>
2019-11-15 15:34:08 -0500

    ulrs.py remove slash after results

--- model1/urls.py
+++ model1/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [ # create one url pattern for each page
     path('', views.home, name = 'home'),
-    path('results/', views.results, name = 'results')
+    path('results', views.results, name = 'results')
     ]

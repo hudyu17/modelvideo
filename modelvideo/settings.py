3a9f0660e3128e7885814bd654aab02c9921cf38
hudyu17 <hengjeung.yuen@gmail.com>
2019-11-15 15:23:13 -0500

    debug true lol

--- modelvideo/settings.py
+++ modelvideo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_ecljx202fwlq+0-26r5ke&r-wncnq9!&rz+vg4(t4lh9o6&#l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['mlbsearch.herokuapp.com', 'localhost']
 
@@ -130,10 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, '/model1/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/model1/static/'
+# STATIC_URL = '/static/'
+STATIC_URL = '‪/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
